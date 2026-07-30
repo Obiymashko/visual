e5ebc47bc1cbcbb12cf24f18b56c4ffef5a16edf
Obiymashko <91619927+Obiymashko@users.noreply.github.com>
2026-07-30 13:53:57 +0300

    Refactor app.py to remove unnecessary imports

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 import streamlit as st
 
-# Використовуємо старе ім'я файла, щоб Streamlit його знайшов, але новий заголовок "Музейний реєстр"
 page1 = st.Page(
     "pages/1_🏛️_Нерухомі_пам'ятки.py",
     title="Нерухомі пам'ятки",
-    icon="🏛️",
     default=True,
 )
 page2 = st.Page(
-    "pages/2_🖼️_Музеї_РМФУ.py", title="Музейний реєстр", icon="🖼️"
+    "pages/2_🖼️_Музеї_РМФУ.py",
+    title="Музейний реєстр",
 )
 
 pg = st.navigation([page1, page2])
