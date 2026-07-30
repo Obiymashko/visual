a2cbbb71b6566d7306495165c104613d3ae05d62
Obiymashko <91619927+Obiymashko@users.noreply.github.com>
2026-07-30 13:29:27 +0300

    Refactor page definitions in app.py
    Updated page titles and paths in Streamlit app.

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 import streamlit as st
 
-# Вказуємо ТОЧНІ шляхи до ваших файлів, включаючи емодзі та кирилицю
-page1 = st.Page("pages/1_🏛️_Нерухомі_пам'ятки.py", title="Нерухомі пам'ятки", icon="🏛️", default=True)
-page2 = st.Page("pages/2_🖼️_Музеї_РМФУ.py", title="Музеї РМФУ", icon="🖼️")
+# Вказуємо ТОЧНІ шляхи до ваших файлів сторінок
+page1 = st.Page(
+    "pages/1_🏛️_Нерухомі_пам'ятки.py",
+    title="Нерухомі пам'ятки",
+    icon="🏛️",
+    default=True,
+)
+page2 = st.Page(
+    "pages/2_🖼️_Музейний_реєстр.py", title="Музейний реєстр", icon="🖼️"
+)
 
 pg = st.navigation([page1, page2])
 pg.run()
